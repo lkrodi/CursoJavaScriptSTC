0ca8a53df21d77e66c4b354820921a06e8c7148a
Rodolfo Escalante Cumpa <rodolfokenlly@gmail.com>
2017-11-05 16:40:56 -0500

    Curso introducturio para el STC-Cueto

---
+++ main.js
@@ -0,0 +1,59 @@
+/*var lista_integrantes = [
+    'Emymi',
+    'Rodolfo',
+    'Nicolas',
+    'Christian'
+];
+
+var direccion = 'Independencia';
+lista_integrantes.push(direccion);
+
+console.log(lista_integrantes[3]);
+document.write(lista_integrantes[2],'<br>');
+
+for(var i=0; i<lista_integrantes.length; i++){
+    console.log(lista_integrantes[i]);
+    document.write(lista_integrantes[i], '<br>');
+}
+/*
+for (var index = 0; index < 100; index++) {
+    document.write('Hola mundo 5 millones','<br>');
+}*/
+/*
+var lista_futbol={
+    'Delantero':'Miguel',
+    'Mediocampista':'Patrick',
+    'Defensa':'Christian',
+    'Arquero':'Aldair',
+    'Entrenador':'Witer',
+}
+
+document.write(lista_futbol.Arquero);
+console.log(lista_futbol.Defensa);
+for(var propiedad in lista_futbol){
+    console.log(lista_futbol[propiedad]);
+}
+
+
+var yo = {
+    'Nombre' : 'Christian Rios Sanchez',
+    'DNI' : 756600789,
+    'Edad' : 19,
+    'Estudios' : 'I.S.E.T.P Carlos Cueto Fernandini',
+    'GrupodeEstudios' : 'Student Tech Club',
+    'Fechadenacimiento' : {
+        'Dia' : 13,
+        'Mes' : 05,
+        'Año' : 1998
+    },
+    'Hoby' : {
+        '1' : 'Futbol',
+        '2' : 'Canto',
+        '3' : 'Baile', 
+    }
+}
+
+console.log(yo.Fechadenacimiento);*/
+
+
+
